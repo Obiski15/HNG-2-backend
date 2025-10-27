b781045359a1e490ba7af6fea359109d08abad75
Obiski15 <obiski15@gmail.com>
2025-10-27 12:43:17 -0700

    fix: prod build path

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
+// prettier-ignore-start
+import "module-alias/register"
+// prettier-ignore-end
+
 import "@/config/load-env"
 
 import app from "./app"
 import config from "./config"
 import logger from "./utils/logger"
 
-;(BigInt.prototype as any).toJSON = function () {
-  return this.toString()
-}
+;(BigInt.prototype as unknown as { toJSON: () => string }).toJSON =
+  function () {
+    return this.toString()
+  }
 
 app.listen(config.port, () => {
   logger.info(`Server is running on port ${config.port}`)
